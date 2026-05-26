3cb4c89ab5b7e3257912e789de783df7ecb78286
bastianaf <bastian.avila@pucv.cl>
2026-05-25 23:18:12 -0400

    Remove projects/startup from snacks dashboard (needs lazy.nvim)

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -301,8 +301,6 @@ Y88888888888888888888888888888888888888888888888P
 			{ section = 'header' },
 			{ section = 'keys', gap = 1, padding = 1 },
 			{ icon = ' ', title = 'Recent files', section = 'recent_files', indent = 2, padding = 1 },
-			{ icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-			{ section = 'startup' },
 		},
 	},
 })
